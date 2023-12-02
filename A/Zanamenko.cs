6b92a57a64d9285b69534db35a998da31c526ed3
Miroslav Netik <netik.mira.f@gmail.com>
2023-12-02 06:27:28 +0100

    Pokusy s polem.

--- A/Zanamenko.cs
+++ A/Zanamenko.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace A
 {
@@ -10,21 +6,47 @@ namespace A
     {
         public static void Mainx(string[] args)
         {
-            int i, znamenko;
-            Console.WriteLine("Zadej číslo: ");
-            i = int.Parse(Console.ReadLine());
-
-            znamenko = 0;
-            if (i < 0)
-                znamenko = -1;
-            if (i == 0)
-                znamenko = 0;
-            if (i > 0)
-                znamenko = 1;
-            Console.WriteLine("Znaménko čísla je {0} (-1: zap., 0:nula, 1:klad)", znamenko);
             {
+                int i, znamenko;
+                Console.WriteLine("Zadej číslo: ");
+                i = int.Parse(Console.ReadLine());
 
+                znamenko = 0;
+                if (i < 0)
+                    znamenko = -1;
+                if (i == 0)
+                    znamenko = 0;
+                if (i > 0)
+                    znamenko = 1;
+                Console.WriteLine("Znaménko čísla je {0} (-1: zap., 0:nula, 1:klad)", znamenko);
+            }
+            {
+                int[] pole_1 = { 1, 2, 3, 4, 5, };
+                foreach (var a in pole_1)
+                {
+                    Console.WriteLine(a);
+                }
+                Console.WriteLine(pole_1[0]);
+            }
+            {
+                int[] pole_2 = { 1, 2, 3 };
+                for (int i = 0; i < pole_2.Length; i++)
+                    Console.WriteLine(pole_2[i] + 100);
+            }
+            {
+                string s = "Hello";
+                foreach (char ch in s) Console.WriteLine(ch);
+            }
+            {
+                int[] pole_3 = new int[5];
+                pole_3[0] = 100;
+                pole_3[1] = 1000;
+
+                Array.Sort(pole_3);
+                Console.WriteLine("Pozice čísla " + 100 + " je " + Array.BinarySearch(pole_3, 100));
+                //Console.WriteLine("Pozice čísla" + cislo + "je" + Array.BinarySearch(pole_2, cislo);
             }
         }
+
     }
 }
