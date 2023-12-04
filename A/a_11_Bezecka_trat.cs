8015bf3ededc22c9ea27064d489d93548f4b9602
Miroslav Netik <netik.mira.f@gmail.com>
2023-12-04 21:10:21 +0100

    Sekce A - hotovo.

--- A/a_11_Bezecka_trat.cs
+++ A/a_11_Bezecka_trat.cs
@@ -11,6 +11,20 @@ namespace A
         //  a_11) Sestavte program, který se zeptá na délku běžecké trati v metrech a poté na čas běžce na
         //  této trati ve vteřinách. Poté zobrazí, jakou průměrnou rychlostí v [km/hod] běžec běžel. (program Doc. Homoly)¨
         public static void Mainx(string[] args)
-        { }
+        {
+            Console.WriteLine("Jsem a_11.");
+
+            const float konstanta = (float)3.6;
+            float trat, cas, vysledek;
+            Console.WriteLine("Zadej délku trati v metrech: ");
+            trat = float.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej dobu běhu v sekundách: ");
+            cas = float.Parse(Console.ReadLine());
+            vysledek = (trat / cas) * konstanta;
+            Console.WriteLine($"Průměrná rychlost běžce v je {vysledek} km/h.");
+
+            Console.WriteLine("Press Enter...");
+            Console.ReadLine();
+        }
     }
 }
--- A/a_13_tezsi_pravouhly_trojuhelnik.cs
+++ A/a_13_tezsi_pravouhly_trojuhelnik.cs
@@ -12,6 +12,20 @@ namespace A
         //  Pro kontrolu: zadáte-li odvěsny o délce 3 a 4, pak výsledek je 5 (přesněji 5,0, protože výsledek odmocniny
         //  se musí ukládat do desetinného typu )
         public static void Mainx(string[] args)
-        { }
+        {
+            Console.WriteLine("Jsem a_13");
+
+            float prepona, odvesna_1, odvesna_2;
+
+            Console.WriteLine("Zadej délku první odvěsny: ");
+            odvesna_1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej délku druhé odvěsny: ");
+            odvesna_2 = float.Parse(Console.ReadLine());
+            prepona = (float)Math.Sqrt(odvesna_1 * odvesna_1 + odvesna_2 * odvesna_2);
+            Console.WriteLine($"Přepona trojúhelníku je {prepona} dlouhá.");
+
+            Console.WriteLine("Press Enter...");
+            Console.ReadLine();
+        }
     }
 }
