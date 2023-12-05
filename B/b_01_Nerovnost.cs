556e387a09216e3745e798a1338a0afe9ef2963e
Miroslav Netik <netik.mira.f@gmail.com>
2023-12-05 15:03:15 +0100

    Rozpracované b_01.

--- B/b_01_Nerovnost.cs
+++ B/b_01_Nerovnost.cs
@@ -9,6 +9,20 @@ namespace B
     internal class b_01_Nerovnost
     {
         public static void Mainx(string[] args)
-        { }
+        {
+            //b_01) Určete, zda dvě zadaná
+            //čísla x,y splňují nerovnost x+3 < 5y-1
+            Console.WriteLine("Jsem b_01.");
+
+            float cislo_x, cislo_y;
+            Console.WriteLine("Zadej číslo x: ");
+            cislo_x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej číslo y: ");
+            cislo_y = float.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Press Enter... ");
+            Console.ReadLine();
+        }
     }
 }
