3c160fff9d871e20c251b4db80d7d72b65069bf2
Miroslav Netik <netik.mira.f@gmail.com>
2023-12-12 17:05:40 +0100

    Doděláno F1_09.

---
+++ F1/F1_09_Obrat_text.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1
+{
+    internal class F1_09_Obrat_text
+    {
+        /* 9) Sestavte funkci ObratText s jedním parametrem - textovým řetězcem. Výsledkem volání funkce
+    'je tentýž textový řetězec, ale má znaky v obráceném pořadí.
+    'Funkci ověřte na zadání: sestavte program, který načte textový řetězec a zobrazí ho v
+    'obráceném pořadí znaků.*/
+        public static void Mainx(string[] args)
+        {
+            Console.WriteLine("Jsem F01_09\n");
+
+            string zadany_text, obraceny_text, sub_obraceny_text = "";
+
+            Console.WriteLine("Zadej slovo pro obracení: ");
+            zadany_text = Console.ReadLine();
+            obraceny_text = F_ObratText(zadany_text);
+            Console.WriteLine($"Funkce -> {obraceny_text}");
+            S_ObratText(zadany_text, ref sub_obraceny_text);
+            Console.WriteLine($"Subrutina -> {obraceny_text}");
+
+            Console.WriteLine("\nStiskni cokoliv...");
+            Console.ReadKey();
+        }
+        // Následuje funkce a subrutina
+        static string F_ObratText(string text)
+        {
+            // Obrátí pořadí písmen ve slově (text)
+            string obraceny = "";
+            char pismeno;
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                pismeno = text[i];
+                obraceny += pismeno.ToString();
+            }
+            return obraceny;
+        }
+        static void S_ObratText(string text, ref string obraceny_vystup)
+        {
+            // Obrátí pořadí písmen ve slově (text)
+            string obraceny = "";
+            char pismeno;
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                pismeno = text[i];
+                obraceny += pismeno.ToString();
+            }
+            obraceny_vystup = obraceny;
+        }
+    }
+}
--- F1/Program.cs
+++ F1/Program.cs
@@ -11,7 +11,8 @@ namespace F1
         static void Main(string[] args)
         {
             //F1_04_Prestupny_rok_gregoriansky.Mainx(args);
-            F1_08_Pocet_dni_mesice.Mainx(args);
+            //F1_08_Pocet_dni_mesice.Mainx(args);
+            F1_09_Obrat_text.Mainx(args);
         }
     }
 }
