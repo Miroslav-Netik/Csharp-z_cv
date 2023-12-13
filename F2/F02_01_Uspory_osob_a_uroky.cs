ca93615ba182b74903059d91384b0f38c472ca46
Miroslav Netik <netik.mira.f@gmail.com>
2023-12-13 13:54:36 +0100

    Doděláno F2 - 02.

--- F2/F02_01_Uspory_osob_a_uroky.cs
+++ F2/F02_01_Uspory_osob_a_uroky.cs
@@ -13,9 +13,11 @@ namespace F2
             Console.WriteLine("Jsem F02_01");
 
             double[] pole_uspor = new double[100];
+            double[] vystupni_pole = new double[100];
+            //double[] pole_urokovane = new double[];
             double zadana_uspora, zadany_urok;
             int pocitadlo = 0;
-            string txt_vystup = "", txt_vystup_z_pole = "";
+            string txt_vystup = "", txt_vystup_z_pole = "", txt_sub_vystup_z_pole = "";
 
             Console.WriteLine("Zadej úrok: ");
             zadany_urok = double.Parse(Console.ReadLine());
@@ -29,13 +31,13 @@ namespace F2
                 Console.WriteLine("Zadej uspořenou částku (0 pro konec): ");
                 zadana_uspora = double.Parse(Console.ReadLine());
             }
-
+            //*************
             for (int j = 0; j < pocitadlo; j++) // Vypíše obsah pole
             {
                 txt_vystup += $"{pole_uspor[j]}, ";
             }
             Console.WriteLine(txt_vystup);
-
+            //*************
             double[] pole_urokovane = F_SpoctiUrok(pole_uspor, zadany_urok); // Volá funkci uročení
             for (int j = 0; j < pocitadlo; j++) // Vypíše  obsah úročeného pole funkcí
             {
@@ -43,11 +45,20 @@ namespace F2
             }
             Console.WriteLine($"Funkce - > {txt_vystup_z_pole}");
 
+            S_SpoctiUrok(pole_uspor, zadany_urok, vystupni_pole); // Volá subrutinu úročení
+            for (int i = 0; i < pocitadlo; i++)
+            {
+                txt_sub_vystup_z_pole += $"{vystupni_pole[i]}, ";
+            }
+            Console.WriteLine($"Subrutina - > {txt_sub_vystup_z_pole}");
+
             Console.WriteLine("\nStiskni cokoliv...");
             Console.ReadKey();
         }
         static double[] F_SpoctiUrok(double[] pole, double urok)
         {
+            // Funkce pro výpočet úroku
+            // Vrátí pole úročenými hodnotami
             double[] vnitrni_pole = new double[pole.Length];
             double odkladaci, prepocet_uroku;
 
@@ -60,6 +71,23 @@ namespace F2
             }
             return vnitrni_pole;
         }
-       
+        static void S_SpoctiUrok(double[] pole, double urok, double[] vystup_pole)
+        {
+            // Subrutina pro výpočet úroku
+            // Vrátí pole úročenými hodnotami
+            double[] vnitrni_pole = new double[pole.Length];
+            double odkladaci, prepocet_uroku;
+
+            prepocet_uroku = urok / 100;
+
+            for (int i = 0; i < pole.Length; i++)
+            {
+                odkladaci = pole[i] + pole[i] * prepocet_uroku;
+                vnitrni_pole[i] = odkladaci;
+                vystup_pole[i] = odkladaci;
+            }
+            //Array.Copy(vnitrni_pole, vystup_pole, vnitrni_pole.Length);
+            
+        }
     }
 }
---
+++ F2/F2_02_Losovani_sazky.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F2
+{
+    internal class F2_02_Losovani_sazky
+    {
+        public static void Mainx(string[] args)
+        {
+            /* 2) Stroj na losování Sazky se porouchal. Tahal sice z osudí čísla 1 až 49, ale některá opakovaně. Vytvořte funkci,
+        'které se předá pole takovýchto čísel, funkce pole vrátí očištěné od druhých a dalších výskytů opakujících se čísel
+        '(pro jednoduchost stačí, aby na jejich místě dosadila nulu). Nevadí, že tedy může být ve výsledku méně čísel než požadovaných pět.*/
+        }
+    }
+}
